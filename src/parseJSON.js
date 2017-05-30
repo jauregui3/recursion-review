63024297fcdd5264307c5ee2a9920f5f06ebb9e9
Juancarlos Jauregui <jauregui3@gmail.com>
2017-05-30 16:58:55 -0700

    Pseudocode basic structure of implementation

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,26 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+
+  // parse strings
+  // parse numbers
+  // parse arrays
+  // parse objects
+  // parse booleans
+  // parse null
+
+  // apply appropriate parse function to each type of object
+    // if string
+      // return parse strings function
+    // if number
+      // return parse numbers function
+    // if array
+      // return parse arrays function
+    // if object
+      // return parse objects function
+    // if boolean
+      // return parse booleans function
+    // if null
+      // return parse null function
+
 };
